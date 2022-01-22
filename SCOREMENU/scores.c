9aaf7c406dc92b5e142a11304eda8b554d4b0816
ohsoroso <notfuzzeh@gmail.com>
2022-01-22 06:32:56 -0600

    work on mostly score and starting primes

--- SCOREMENU/scores.c
+++ SCOREMENU/scores.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Declare functions
-void display_menu(void);
-void game_result(void);
-void list_scores(void);
-void games_won(void);
-void scores_sort(void);
+// Declare functions
+void game_result(int array [] [3]);
+void list_scores(int array [] [3]);
+void games_won(int array [] [3]);
+void scores_sort(int array [] [3]);
 
 int main() {
-    int teams = 2, score = 100, game [teams] [score], i, j;
-    
-    //Call menu
-    display_menu();
+int game [2] [3], i, j;
+char choice;
 
-    return 0;
-}
-
-//Display menu options
-void display_menu(void) {
-    int teams = 2, score = 100, game [teams] [score], i, j;
-    char choice;
-    
-    do
-    {
+  do {
         printf("\n\n************************* \n");
         printf("**      MAIN MENU     *** \n");
         printf("************************* \n\n");
@@ -38,14 +26,14 @@ void display_menu(void) {
         // set menu options
         switch(choice)
         {
-            case 'A': game_result();
+            case 'A': game_result(game);
                 break;
-            /*case 'B': list_scores();
+            case 'B': list_scores(game);
                 break;
-            case 'C': games_won();
+            case 'C': games_won(game);
                 break;
-            case 'D': scores_sort();
-                break;*/
+            case 'D': scores_sort(game);
+                break;
             case 'E': printf("Quitting program!\n");
                 exit(0);
                 break;
@@ -54,18 +42,61 @@ void display_menu(void) {
         }
         
     } while (choice != 'E');
-    
+return 0;
 }
 
-//Begin function items
-void game_result(void) {
-int teams = 2, score = 100, game [teams] [score], i, j;
-    
-    printf("Enter your stuf: \n");
-    for (i = 0; i < teams; i++)
-      for (j = 0; j < score; j++) {
-        scanf("%d", &game [i] [j]);
-        break;
+void game_result(int array [] [3]) {
+    int i, j;
+    printf("Enter scores: \n");
+    for (i = 0; i < 2; i++) {
+      for (j = 0; j < 3; j++) {
+        scanf("%d", &array [i] [j]);
       }
-    return;
+    }
+  return;
+}
+
+void list_scores(int array [] [3]) {
+    int i, j, win = 0, loss = 0, tie = 0;
+    for (j = 0; j < 3; j++) {
+    if (array [0] [j] < array [1] [j]) {
+        loss++;
+    }
+    if (array [0] [j] > array [1] [j]) {
+        win++;
+      }
+    if (array [0] [j] == array [1] [j]) {
+        tie++;
+    }
+  }
+  printf("Wins: %d\nLosses: %d\nTies: %d", win, loss, tie);
+  return;
+}
+
+void games_won(int array [] [3]) {
+    int i, j, winset [2] [3];
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        if (array [0] [j] > array [1] [j]) {
+        winset [i] [j] = array [i] [j];
+      }
+    }
+  }
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 3; j++) {
+    printf("%d\n", winset [i] [j]);
+    }
+  }
+  return;
+}
+
+void scores_sort(int array [] [3]) {
+    int i, j, score [3];
+    for (i = 1; i < 2; i++) {
+      for (j = 0; j < 3; j++) {
+        if (array [1] [j] < array [1] [j + 1]) {
+          score [j] = array [i] [j];
+        }
+      }
+    }
 }
