cca5d1ab8910c4c53592d3ddff54c1a79159afca
ohsoroso <notfuzzeh@gmail.com>
2022-01-22 07:26:56 -0600

    quick project nothing crazy

---
+++ IFANDOR/ifscenario.py
@@ -0,0 +1,13 @@
+rainySeason = ["May", "June", "July", "August", "September", "October", "November"]
+hurricaneSeason = ["June", "July", "August", "September", "October", "November"]
+springBreak = ["Feburary", "March", "April"]
+season = input("What month is it?")
+deal = input("Are tickets on sale?")
+
+
+if season == (rainySeason) and (hurricaneSeason) and springBreak or (deal == "Yes"):
+  print("Time to get those tickets !!!.")
+elif season != (rainySeason) and (hurricaneSeason) and springBreak or (deal == "No"):
+  print("Time to get those tickets !!!.")
+else:
+  print("Sorry maybe next time :(")
