b858bacf044fcabefcd73092582df6b9d891797a
ohsoroso <notfuzzeh@gmail.com>
2022-01-23 16:57:59 -0600

    done and commented yay

--- BANKMENU/banking.c
+++ BANKMENU/banking.c
@@ -1,91 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *xp, int *yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
+//Declare functions
+void game_result(int array [] [3]);
+void list_scores(int array [] [3]);
+void games_won(int array [] [3]);
+void scores_sort(int array [] [3]);
 
-void bubbleSort(int arr[], int n) {
-int i, j;
-   for (i=0; i < n-1; i++)      
-
+int main() {
+int game [2] [3], i, j;
+char choice;
+  //Set menu
+  do {
+        printf("\n\n************************* \n");
+        printf("**      MAIN MENU     *** \n");
+        printf("************************* \n\n");
+        printf("[A]Enter game result \n");
+        printf("[B]Current Record(# of wins and # of losses and # of ties \n");
+        printf("[C]Display ALL results from all games WON \n");
+        printf("[D]Display ALL results ordered by oponent score from low to high. \n");
+        printf("[E]Quit the program \n");
+        printf("Enter an option: ");
+        scanf(" %c",&choice);
         
-       for (j=0; j < n-i-1; j++) 
-           if (arr[j] < arr[j+1])
-              swap(&arr[j], &arr[j+1]);
+        //Set menu options
+        switch(choice)
+        {
+            case 'A': game_result(game);
+              break;
+            case 'B': list_scores(game);
+              break;
+            case 'C': games_won(game);
+              break;
+            case 'D': scores_sort(game);
+              break;
+            case 'E': printf("Quitting program!\n");
+        }
+        
+    } while (choice != 'E');
+    return 0;
 }
-
-void printArray(int arr[], int size) {
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", arr[i]);
+//Store result for you and your opp
+void game_result(int array [] [3]) {
+    int i, j;
+    printf("Enter team scores: \n");
+    for (i = 0; i < 1; i++) {
+      for (j = 0; j < 3; j++) {
+        scanf("%d", &array [i] [j]);
+    }
+  }
+  printf("Enter opponent scores: \n");
+    for (i = 1; i < 2; i++) {
+      for (j = 0; j < 3; j++) {
+        scanf("%d", &array [i] [j]);
+    }
+  }
+  return;
+}
+//Find wins, losses, and ties
+void list_scores(int array [] [3]) {
+    int i, j, win = 0, loss = 0, tie = 0;
+    for (j = 0; j < 3; j++) {
+    if (array [0] [j] < array [1] [j]) {
+        loss++;
+    }
+    if (array [0] [j] > array [1] [j]) {
+        win++;
+      }
+    if (array [0] [j] == array [1] [j]) {
+        tie++;
+    }
+  }
+  printf("Wins: %d\nLosses: %d\nTies: %d", win, loss, tie);
+  return;
+}
+//Prints out the scores from only games won by you
+void games_won(int array [] [3]) {
+    int i, j;
+    for (i = 0; i < 2; i++) {
+      for (j = 0; j < 3; j++) {
+        if (array [0] [j] > array [1] [j]) {
+        printf("%d ", array [i] [j]);
+      }
+    }
     printf("\n");
+  }
+  return;
 }
-
-int main(void) {
-  int deposit, sum = 0, totalSum = 0, avrg = 0, i = 0, j = 0, n, low;
-  int deposits [5];
-  char choice;
-do {
-printf("rosocorp ATM Services how may we help you today ?\n");
-printf("[G]et a new deposit\n");
-printf("[S]um of all deposits\n");
-printf("[D]eposits will be displayed from highest to lowest deposit\n");
-printf("[A]verage of all deposits\n");
-printf("[L]owest deposit will be displayed\n");
-printf("[Q]uit the program\n");
-scanf("%s", &choice);
-
-switch (choice) {
-case 'G':
-  printf("Enter deposit amount:");
-  scanf("%d", &deposit);
-  printf("Your deposit was: %d\n", deposit);
-  for (i = i; i < 5; i++) {
-      deposits[i] = deposit;
-      break;
+//Sorts opps scores from lowest to highest
+void scores_sort(int array [] [3]) {
+    int i, j, temp, score [3];
+    for (i = 1; i < 2; i++) {
+      for (j = 0; j < 3; j++) {
+        score [j] = array [i] [j];
   }
-  i++;
-  break;
-
-case 'S':
-  for (i = 0; i < 5; i++) {
-      sum += deposits[i];
-  }
-  printf("\nThe sum of all deposits is: %d\n", sum);
-  break;
-  
-case 'D':
-n = sizeof(deposits)/sizeof(deposits[0]);
-bubbleSort(deposits, n);
-printf("Deposits highest to lowest: \n");
-printArray(deposits, n);
-  break;
-  
-case 'A':
-  for (i = 0; i < 5; i++) {
-      totalSum += deposits[i];
-  }
-  avrg = totalSum / 5;
-  printf("\nAverage is: %d\n", avrg);
-  break;
-  
- case 'L':
- n = sizeof(deposits)/sizeof(deposits[0]);
- low = deposits[0];
-   for ( i = 0; i < n; i++) {
-       if (deposits[i] < low) {
-           low = deposits[i];
-       }
-   }
-   printf("The lowest deposit was: %d\n", low);
-   break;
-
-default:
-  break;
 }
-} while (choice != 'Q');
-  return 0;
+    for (j = 0; j < 3; j++) {
+      for (i = j + 1; i < 3; i++) {
+        if (score [j] > score [i]) {
+          temp = score [j];
+          score [j] = score [i];
+          score [i] = temp;
+        }
+      }
+    }
+    for (j = 0; j < 3; j++) {
+      printf("%d", score [j]);
+  }
+  return;
 }
